4368dab627fbaebf1382e7d5b75331b276eabef6
Kelum Senanayake <kelumkps@yahoo.com>
2016-12-23 21:32:24 +0800

    Refactored uninstall & operation modules

--- git-crucible-uninstall.js
+++ git-crucible-uninstall.js
@@ -14,22 +14,9 @@ program
     .alias('un')
     .parse(process.argv);
 
-promptUserForConfirmation(function(location) {
+operations.promptUserForConfirmation('uninstall', {
+    description: module.exports.description,
+    version: module.exports.version
+}, function(location) {
     operations.uninstall(location);
 });
-
-function promptUserForConfirmation(done) {
-    operations.findSelectedProject(function(location) {
-        if (location && location !== null) {
-            operations.handleLocationFound(location, done, 'uninstall');
-        } else {
-            logo.display(module.exports.description, module.exports.version);
-            operations.handleLocationNotFound(function(userInput) {
-                operations.handleLocationFound(userInput, done, 'uninstall');
-            }, 'uninstall');
-        }
-    }, {
-        description: module.exports.description,
-        version: module.exports.version
-    });
-}
